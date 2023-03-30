7fd6994805d04d06e51fe65023a0a692977cc2ed
GoldTryck <lariosh69@gmail.com>
2023-03-30 08:07:24 -0600

    base de datos y funcion agregar libro

--- gestion_bib.c
+++ gestion_bib.c
@@ -3,60 +3,81 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-struct libro{
-    char titulo[50];
-    char autor[50];
+#include <ctype.h>
+typedef struct libro{
+    char titulo[MAX_LEN];
+    char autor[MAX_LEN];
     int stock;
     int borrowed;
-
-};
-char* leer_cadena() {
+    int indice;
+} libro;
+char* leer_cadena(char* mensaje) {
     char buffer[MAX_LEN];
-    char* cadena = NULL;
-    int longitud = 0;
-    int caracter;
-
+    char* chain = NULL;
+    int length = 0;
+    printf("%s",mensaje);
     fgets(buffer, MAX_LEN, stdin);
-
-    // Eliminar el caracter de nueva línea del final de la cadena leída
+    
+    // Eliminar el espacio en blanco y caracter de nueva línea del final de la cadena leída
+    buffer[strcspn(buffer," ")] = '_';
     buffer[strcspn(buffer, "\n")] = '\0';
-
+    
     // Obtener la longitud de la cadena
-    longitud = strlen(buffer);
+    length = strlen(buffer);
 
     // Asignar memoria para la cadena
-    cadena = (char*)malloc((longitud + 1) * sizeof(char));
+    chain = (char*)malloc((length + 1) * sizeof(char));
 
     // Copiar la cadena leída al puntero asignado
-    strcpy(cadena, buffer);
-
+    strcpy(chain, buffer);
+    //Convertir la cadena a MAYUSC
+    for(int i = 0; chain[i] != '\0'; ++ i){
+        chain[i] = toupper(chain[i]);
+    }
     // Limpiar el buffer
     fflush(stdin);
-
-    return cadena;
+    
+    return chain;
 }
-void agregar_libro(struct libro *biblioteca){
-    int i = 0;
-    printf("\tADD BOOK MANAGER\n"); printf("Registre el titulo del libro: ");
-    char* title_to_add = leer_cadena();
-    for(i = 0;i<100;i++){
-        if(biblioteca[i].titulo == '\0'){
-            strcpy(title_to_add, biblioteca[i].titulo);
-            printf("\n%s\n",biblioteca[i].titulo);
-            printf("\nhola hola\n");
-            return;
+void agregar_libro(int* contador, libro* biblioteca){
+    printf("\tADD BOOK MANAGER\n");
+    while(1){
+        FILE* dataBase;
+        if(*contador == 0) dataBase = fopen("DataBase.txt","w");
+        else dataBase = fopen("DataBase.txt","a");
+        libro aux;
+        char* cadena = leer_cadena("Registre el titulo del libro: ");
+        strcpy(aux.titulo, cadena); free(cadena);
+        cadena = leer_cadena("Registre el nombre del autor: ");
+        strcpy(aux.autor, cadena); free(cadena);
+        //Falta agregar funcion leerNum
+        aux.stock = 5; aux.borrowed = 2; aux.indice = *contador;
+        biblioteca[*contador] = aux;
+        fprintf(dataBase,"%d %s %s %d %d\n",aux.indice, aux.titulo, aux.autor, aux.stock, aux.borrowed);
+        fclose(dataBase);
+        (*contador) ++;
+        char* rep = leer_cadena("Desea registrar otro libro: Escriba 'Si' para continuar, 'No' para regresar al menu:\t");
+        if(strcmp(rep,"NO") == 0){
+            free(rep);
+            break;
         }
+        free(rep);
     }
-    free(title_to_add);
+}
+libro buscarLibro(libro* biblioteca){
+    libro aux;
+
+    return aux;
 }
 int main(){
-    struct libro biblioteca[5];
-    memset(biblioteca,'\0',sizeof(biblioteca));
-    if(strcmp(biblioteca[0].titulo,'\0') == 0){
-        strcpy(biblioteca[0].titulo,"hola");
-        printf("%s",biblioteca[0].titulo);
-    }
-    
+    FILE* dataBase = fopen("DataBase.txt","r");
+    libro bib[100];
+    int count =0; 
+    while (fscanf(dataBase, "%d %s %s %d %d", &bib[count].indice, bib[count].titulo, bib[count].autor, &bib[count].stock, &bib[count].borrowed) == 5){
+        count ++;
+    };
+    agregar_libro(&count,bib);
+    fclose(dataBase);
     return 0;
 }
 /*
